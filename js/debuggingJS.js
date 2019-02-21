1c09f1c8fcd2258a4633b63de26ef544fb74dc7e
Osamu <oskatagiri@gmail.com>
2019-02-21 13:20:19 -0600

    get response header

--- js/debuggingJS.js
+++ js/debuggingJS.js
@@ -12,18 +12,25 @@ http://trefle.io/api/plants/138738/?token=SWVENEduMFMxellqNHFIM3RmL0c4Zz09
 */
 
 const CORSproxy   = 'https://cors.io/';
-const trefleToken = 'SWVENEduMFMxellqNHFIM3RmL0c4Zz09';
-const url         = 'https://trefle.io/api/plants/?token=';
+const trefleToken = '/?token=SWVENEduMFMxellqNHFIM3RmL0c4Zz09';
+const url         = 'https://trefle.io/api/plants';
 fullURL           = CORSproxy + '?' + url + trefleToken;
 
+/*
 $.getJSON(fullURL, function(response){
     console.log(response);
     document.getElementById("DebugCont").innerHTML = response;
 });
-
-/*
-Http.onreadystatechange=(e)=>{
-    document.getElementById("DebugCont").innerHTML = Http.responseText;
-}
 */
+
+var Http = new XMLHttpRequest();
+Http.onreadystatechange=(e)=>{
+    
+    console.log(Http.getAllResponseHeaders);
+    document.getElementById("resHeader").innerHTML = Http.getAllResponseHeaders;
+    
+    console.log(Http.responseText);
+    document.getElementById("resText").innerHTML = Http.responseText;
+    
+}
 // document.getElementById("DebugCont").innerHTML = "New text!";
