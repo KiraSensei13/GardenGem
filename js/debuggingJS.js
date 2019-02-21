90ac0cdb3856e7597519a1ab3bccee133648fe08
Osamu <oskatagiri@gmail.com>
2019-02-21 08:05:00 -0600

    start getting JSON from Trefle API (in progress)

---
+++ js/debuggingJS.js
@@ -0,0 +1,13 @@
+var url='https://trefle.io/api/plants/138738/?token=SWVENEduMFMxellqNHFIM3RmL0c4Zz09';
+
+$.getJSON(url, function(response){
+    console.log(response);
+    document.getElementById("DebugCont").innerHTML = response;
+});
+
+/*
+Http.onreadystatechange=(e)=>{
+    document.getElementById("DebugCont").innerHTML = Http.responseText;
+}
+*/
+// document.getElementById("DebugCont").innerHTML = "New text!";
